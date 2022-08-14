e2db2dadcf47492952b549d1a4773219bacac733
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-08-14 18:52:18 +0800

    fix: app.js

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const express = require('express')
+const session = require('express-session')
 const cors = require('cors')
 const routes = require('./routes/index')
+const passport = require('./config/passport')
 const helpers = require('./_helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_KEY = 'secret'
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(session({
+  secret: SESSION_KEY,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URL,
+    ttl: 1 * 24 * 60 * 60,
+    autoRemove: 'interval',
+    autoRemoveInterval: 30
+  }),
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
 
 app.use((req, res, next) => {
   res.user = helpers.getUser(req)
   next()
 })
 
-app.use('/', routes)
+app.use('/api', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
