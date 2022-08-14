183fadc72c54ea5d9bf5972e0dc9fee3a2aa7241
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-08-14 18:18:52 +0800

    feat: add basic routes

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
+const cors = require('cors')
+const routes = require('./routes/index')
 const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-// use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
-  // passport.authenticate('jwt', { ses...
-}
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use((req, res, next) => {
+  res.user = helpers.getUser(req)
+  next()
+})
+
+app.use('/', routes)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
