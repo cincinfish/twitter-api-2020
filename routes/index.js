68ae684d5f6ad4b9881d6917f72387b7d4b753ed
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-08-14 18:42:53 +0800

    feat: add user routes & controller

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const users = require('./modules/user')
+const passport = require('../config/passport')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { errorHandler } = require('../middleware/error-handler')
+const userController = require('../controllers/user-controller')
+
+router.post('/signin', passport.authenticate('local', { session: false, failWithError: true }), userController.signin)
+router.post('/admin/signin', passport.authenticate('local', { session: false, failWithError: true }), adminController.signin)
+router.post('/users', userController.signUp)
+
+router.use('/users', authenticated, users)
+
+router.use('/', errorHandler)
 
 module.exports = router
